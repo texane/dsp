65f153a85c7af53ec3ffd98cfb37655ae9e29df7
Fabien Le Mentec <texane@gmail.com>
2012-04-19 00:53:54 -0500

    [ update ] fft source, do_run

--- fft/src/do_run.sh
+++ fft/src/do_run.sh
@@ -3,7 +3,7 @@
 ./a.out > /tmp/mangled_name.o ;
 
 # plot frequency domain
-gnuplot -e "plot '/tmp/mangled_name.o' with impulses; replot; pause mouse key;" ;
+# gnuplot -e "plot '/tmp/mangled_name.o' with impulses; replot; pause mouse key;" ;
 
 # plot time domain
-# gnuplot -e "plot '/tmp/mangled_name.o' with lines; replot; pause mouse key;" ;
+gnuplot -e "plot '/tmp/mangled_name.o' with lines; replot; pause mouse key;" ;
